83a81433cc59e0795d474f8b9e4c7c262cdb24d8
fyrvelm <fyrvelm@gmail.com>
2013-09-15 05:00:31 +0200

    installer + works on windows

--- Main.cs
+++ Main.cs
@@ -1,5 +1,6 @@
 using System;
 using Gtk;
+using System.Diagnostics;
 
 namespace Launcher
 {
@@ -7,6 +8,23 @@ namespace Launcher
 	{
 		public static void Main (string[] args)
 		{
+            PlatformID pid = Environment.OSVersion.Platform;
+            bool Windows = false;
+            switch (pid)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    Windows = true;
+                    break;
+                case PlatformID.Unix:
+                    Windows = false;
+                    break;
+                default:
+                    Windows = false;
+                    break;
+            }
 			Application.Init ();
 			MainWindow win = new MainWindow ();			
 			win.Show ();
